b8fbb281750d264ebb14092a63829434befc3d11
ahmadreza tahmasbi <75021820+ahmadrezatmb@users.noreply.github.com>
2021-08-15 19:37:03 +0430

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'thesongus@gmail.com'
-EMAIL_HOST_PASSWORD = 'Salam@123'
+EMAIL_HOST_PASSWORD = '*'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
